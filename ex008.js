b053757f806d2e23e8bad58ef6271c21838dfb13
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-03-06 12:08:58 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,7 @@
+// console.log('O console Funcionou corretamente');
+
+var vel = 60.5;
+console.log(`A velocidade é ${vel}km/h`);
+if (vel > 80){
+    console.log('Multado');
+}
---
+++ ex009.js
@@ -0,0 +1,8 @@
+var país = 'EUA';
+console.log(`Você vive em ${país}`);
+if(país == 'Brasil' || país == 'brasil'){
+    console.log('Você é BR');
+}else{
+    console.log('Você é Gringo');
+};
+
