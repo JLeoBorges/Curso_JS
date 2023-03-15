86d4bd89c5c89f7ba0053c7c3130490ca56533da
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-03-15 13:39:21 -0300

    Add files via upload

--- analise.js
+++ analise.js
@@ -23,16 +23,13 @@ function inLista(n, l){
 }
 
 
-
-
-
-
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
             valores.push(Number(num.value))
             let item = document.createElement('option');
             item.text = `Valor ${num.value} Adicionado`
             lista.appendChild(item)
+            res.innerHTML = '';
     } else {
         window.alert('Valor inválido ou já registrado na lista.');
 
@@ -40,4 +37,34 @@ function adicionar(){
 num.value = ''
 num.focus();
 
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar!');
+    } else {
+        let tot = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+        let soma = 0;
+        let media = 0;
+        for(let pos in valores){
+            soma += valores[pos];
+
+            if(valores[pos] > maior){
+            maior = valores[pos];
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos];
+            }
+        }
+        media = soma / tot;
+
+        res.innerHTML = '';
+        res.innerHTML += `<p>Ao todo, Temos ${tot} números cadastrados.</p>`;
+        res.innerHTML += `<p>O maior valor adicionado foi ${maior}.</p>`;
+        res.innerHTML += `<p>O menor valor adicionado foi ${menor}.<p>`;
+        res.innerHTML += `<p>A soma dos valores adicionados foi de ${soma}.</p>`;
+        res.innerHTML += `<p>A média dos valores adicionados foi de ${media}.</p>`
+    }
 }
